65b031d289bb15a794ddb20b6002fbec06f047e9
FelixKLG <felix@felixklg.dev>
2024-04-15 17:35:08 +0100

    Optimise for landscape views.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,8 +3,16 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 export const Route = createRootRoute({
     component: () => (
         <>
-            <div className="max-h-screen min-h-screen max-w-screen min-w-screen overflow-clip">
-                <Outlet />
+            <div className="max-h-screen min-h-screen max-w-screen min-w-screen overflow-clip md:flex md:flex-row md:justify-center gap-72">
+                <div className="hidden md:flex items-center justify-center md:pl-20">
+                    <img className="h-screen"
+                        src="/thesurf.gif"
+                        alt="Subway Surfers Attention Grabber"
+                    />
+                </div>
+                <div className="md:pr-20">
+                    <Outlet />
+                </div>
             </div>
         </>
     ),
